e0f0d284c73f62a28bac1bd603a240a69ec32f96
Joel Hallama <joel.hallama@gmail.com>
2020-10-09 17:37:53 +0300

    fixed a bug with multiple arguments

--- checker.c
+++ checker.c
@@ -29,7 +29,7 @@ int		main(int argc, char **argv)
 	}
 	stacks->v = (input == NULL) ? v_check(argv) : v_check(input);
 	stacks->a = (input == NULL) ? init(argc, ++argv) : init(count_elems(input, 0), input);
-	stacks->elems = (input) ? count_elems(input, 1) : 1;
+	stacks->elems = (input) ? count_elems(input, 1) : (size_t)argc - 1;
 	check_duplicates(stacks->a, stacks->elems);
 	init_b(stacks);
 	read_commands(stacks);
