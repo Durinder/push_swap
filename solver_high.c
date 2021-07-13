8c41a63822c848d880040f8e827cd8c4f7d2e3d7
Joel Hallama <joel.hallama@student.hive.fi>
2021-07-13 20:01:13 +0300

    road to working algo

--- solver_high.c
+++ solver_high.c
@@ -1,6 +1,46 @@
 
 #include "push_swap.h"
 
+static int steps_to_co(t_stacks *s, int dir, float co)
+{
+    int i;
+    int counter;
+
+    if (dir == 1)
+        i = 0;
+    else
+        i = s->a_size - 1;
+    counter = 0;
+    while (i < s->a_size && i > -1)
+    {
+        if ((float)s->a[i] < co)
+            return (counter);
+        i += dir;
+        counter++;
+    }
+    return (s->a_size);
+}
+
+static void	push_to_b(t_stacks *s, float co)
+{
+    int r;
+    int rr;
+
+	while (1)
+    {
+        r = steps_to_co(s, 1, co);
+        rr = steps_to_co(s, -1, co);
+        if (r == s->a_size)
+            break ;
+        if (r == 0)
+            redirect_buf(s, s->b, "push");
+        else if (r <= rr)
+            redirect_buf(s, s->a, "rotate");
+        else
+            redirect_buf(s, s->a, "rrotate");
+    }
+}
+
 static float get_median(int *s, int elems)
 {
 	int	    *sorted;
@@ -37,4 +77,5 @@ void    solver_high(t_stacks *s)
 
     median = get_median(s->a, s->a_size);
     ft_printf("%f\n", median);
+    push_to_b(s, median);
 }
