01ab95ac8afa23c139d2c50fb91d13b9a612a299
Joel Hallama <joel.hallama@student.hive.fi>
2021-07-15 17:05:01 +0300

    made int range into 0-n

--- solver_loop.c
+++ solver_loop.c
@@ -2,7 +2,7 @@
 
 //for min value which == 0
 //for max value which == 1
-
+/*
 static int	get_min_or_max(int *s, int size, int which)
 {
 	int	ret;
@@ -37,17 +37,18 @@ static int check_if_done(t_stacks *s, int *p, int elems)
     }
     return (-1);
 }
+*/
 
-static void solver_looper(t_stacks *s, int *p, int elems)
+static void solver_looper(int *slim, t_stacks *s, int *p, int elems)
 {
-    int min;
-    int max;
-    int dist;
+//    int min;
+//    int max;
+//    int dist;
 
-    min = get_min_or_max(p, elems, 0);
-    max = get_min_or_max(p, elems, 1);
-    dist = check_if_done(s, p, elems);
-    while (dist != -1)
+    //min = get_min_or_max(p, elems, 0);
+    //max = get_min_or_max(p, elems, 1);
+//    dist = check_if_done(s, p, elems);
+//   while (dist != -1)
     {
 //       ft_printf("min:%d, max:%d, dist:%d\n", min, max, dist);
 //        if ((p == s->a && p[0] != max && p[0] > p[1]) || 
@@ -67,10 +68,72 @@ static void solver_looper(t_stacks *s, int *p, int elems)
     }
 }
 
+int find_smallest(int *s, int elems, int *cur_min)
+{
+    int i;
+    int candidate;
+    int ret;
+
+    i = 0;
+    candidate = *cur_min;
+    while (i < elems)
+    {
+        if (candidate == *cur_min && s[i] > *cur_min)
+        {
+            candidate = s[i];
+            ret = i;
+        }
+        else if (s[i] < candidate && s[i] > *cur_min)
+        {
+            candidate = s[i];
+            ret = i;
+        }
+        i++;
+    }
+    *cur_min = candidate;
+    if (i == elems)
+        return (-1);
+    return (ret);
+}
+
+int *transform_stack(int *s, int elems)
+{
+    int	*slim;
+	int	i;
+    int cur_min;
+    int min_i;
+
+	slim = ft_memalloc(sizeof(int *) * elems - 1);
+	if (slim == NULL)
+		ft_error_exit("Error: Failed to malloc");
+	i = 0;
+    cur_min = s[0];
+	while (i < elems)
+	{
+        if (s[i] < cur_min)
+            cur_min = s[i];
+		i++;
+	}
+    i = 0;
+    while (i < elems)
+    {
+        min_i = find_smallest(s, elems, &cur_min);
+        slim[min_i] = i;
+        i++;
+    }
+    return (slim);
+}
+
 void    solver_loop(t_stacks *s)
 {
+    int *slim;
 //   ft_printf("halojaa\n");
-    solver_looper(s, s->a, s->a_size);
+    slim = NULL;
+    slim = transform_stack(s->a, s->a_size);
+    solver_looper(slim, s, s->a, s->a_size);
 //    ft_printf("halojaa\n");
-    solver_looper(s, s->b, s->b_size);
+    free(slim);
+    slim = transform_stack(s->b, s->b_size);
+    solver_looper(slim, s, s->b, s->b_size);
+    free(slim);
 }
